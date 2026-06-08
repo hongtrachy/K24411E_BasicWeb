7f24678254fe14f90341c730873066ff1aa9a3d9
Trac Hy <hyhtk24411e@st.uel.edu.vn>
2026-06-08 14:48:26 +0700

    push

--- Product_HTML_DOM/js/main.js
+++ Product_HTML_DOM/js/main.js
@@ -1,11 +1,28 @@
 function load_product_data(dataset,tbl_product)
 {
   var tbody = document.getElementById('product_table_body');
+  tbody.innerHTML = '';
   for(var i=0;i<dataset.length;i++)  
   {
     var row = "<tr onclick='selectProduct(\"" + dataset[i].id + "\", \"" + dataset[i].name + "\", \"" + dataset[i].price + "\")'><td>"+dataset[i].id+"</td><td>"+dataset[i].name+"</td><td>"+dataset[i].price+"</td><td><img src='images/ic_remove.png' alt='Delete' style='cursor:pointer; width:24px; height:24px;' onclick='deleteRow(event)'></td></tr>";
     tbody.innerHTML += row;
   }
+}
+
+function addNewProduct()
+{
+  var newId = document.getElementById('new_product_id').value;
+  var newName = document.getElementById('new_product_name').value;
+  var newPrice = document.getElementById('new_product_price').value;
+  
+  var newProduct = {id: parseInt(newId), name: newName, price: parseFloat(newPrice)};
+  products.push(newProduct);
+  
+  load_product_data(products, document.getElementById('tbl_product'));
+  
+  document.forms['add_product_form'].reset();
+  
+  return false;
 }
 
 function selectProduct(id, name, price)
